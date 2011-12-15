09881a3af865faf1eb144634310beffc9e50c5b9
Jonas Keisel <xJoKe@me.com>
2011-12-15 19:39:44 +0100

    first buggy implementation of api

---
+++ api/config/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$settings = array();
+
+$settings['mysql']['server'] = 'sql.domainteufel.at';
+$settings['mysql']['username'] = base64_decode('ZHQxMDc4');
+$settings['mysql']['password'] = base64_decode('Y2hpZXNvaGM=');
+$settings['mysql']['database'] = 'dt107803';
---
+++ api/v1.0/controller.php
@@ -0,0 +1,14 @@
+<pre>
+<?php
+
+/*require_once "../config/config.inc.php";
+
+mysql_connect($settings['mysql']['server'], $settings['mysql']['username'], $settings['mysql']['password']);
+mysql_select_db($settings['mysql']['database']);*/
+
+//$query = $_GET['q'];
+
+print_r($_GET);
+print_r($_SERVER);
+
+//if (file_exists($_GET[]))
